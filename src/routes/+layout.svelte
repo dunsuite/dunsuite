e7123fd14310d9e6fd54e76b514c05813dab6db9
SpikeySanju <spikeysanju98@gmail.com>
2024-10-11 20:07:47 +0530

    fix font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <Toaster />
-<div class="mx-auto my-2 flex h-screen w-full max-w-2xl flex-col items-center justify-start p-4">
+<div class="mx-auto my-2 flex h-screen w-full max-w-2xl flex-col items-center justify-start p-4 font-InterVariable antialiased">
 	<Header />
 	<slot></slot>
 </div>
