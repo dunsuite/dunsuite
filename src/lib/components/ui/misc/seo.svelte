1c1ce3b2c003c403fbf55b75cf33435d9cc10b98
SpikeySanju <spikeysanju98@gmail.com>
2024-10-11 22:12:36 +0530

    fix seo issue

--- src/lib/components/ui/misc/seo.svelte
+++ src/lib/components/ui/misc/seo.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { SITE_TITLE, SITE_SUBTITLE, SITE_DESCRIPTION } from '$lib/data';
+	import { page } from '$app/stores';
 
 	export let title: string = SITE_TITLE;
 	export let subtitle: string = SITE_SUBTITLE;
 	export let description: string = SITE_DESCRIPTION;
+
+	$: currentUrl = $page.url.href;
 </script>
 
 <svelte:head>
@@ -12,7 +15,7 @@
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
 	<meta property="og:type" content="website" />
-	<meta property="og:url" content={window.location.href} />
+	<meta property="og:url" content={currentUrl} />
 	<meta property="og:image" content="/ogimage.png" />
 	<meta name="twitter:card" content="/ogimage.png" />
 	<meta name="twitter:title" content={title} />
