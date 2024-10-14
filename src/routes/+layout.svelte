3b8ee25cc8e3b79190337d8a6d009b8cb3d4d2ba
SpikeySanju <spikeysanju98@gmail.com>
2024-10-14 21:53:51 +0530

    add tracking scripts for production environment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,36 @@
 	import '../app.css';
 	import { Header, Gradient, Footer } from '$lib/components/ui/misc';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { dev } from '$app/environment';
+	import { env } from '$env/dynamic/public';
 </script>
+
+<svelte:head>
+	{#if !dev}
+		<!-- umami -->
+		<script
+			defer
+			src="https://track.thisux.com/script.js"
+			data-website-id={env.PUBLIC_UMAMI_WEBSITE_ID}
+		></script>
+
+		<!-- clarity -->
+		<script type="text/javascript">
+			(function (c, l, a, r, i, t, y) {
+				c[a] =
+					c[a] ||
+					function () {
+						(c[a].q = c[a].q || []).push(arguments);
+					};
+				t = l.createElement(r);
+				t.async = 1;
+				t.src = 'https://www.clarity.ms/tag/' + i;
+				y = l.getElementsByTagName(r)[0];
+				y.parentNode.insertBefore(t, y);
+			})(window, document, 'clarity', 'script', {env.PUBLIC_CLARITY_TAG_ID});
+		</script>
+	{/if}
+</svelte:head>
 
 <Toaster />
 <div
