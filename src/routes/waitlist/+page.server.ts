72eb61a8b311d6ba812a3ef5f0556e095faefc68
SpikeySanju <spikeysanju98@gmail.com>
2024-10-14 18:11:19 +0530

    fix auth

--- src/routes/waitlist/+page.server.ts
+++ src/routes/waitlist/+page.server.ts
@@ -15,7 +15,6 @@ export const actions: Actions = {
 		}
 
 		// Authenticate as admin
-
 		const authData = await locals.pb.admins
 			.authWithPassword(env.POCKETBASE_ADMIN_EMAIL, env.POCKETBASE_ADMIN_PASSWORD)
 			.catch((err) => {
@@ -23,18 +22,18 @@ export const actions: Actions = {
 				throw error(403, 'Authentication failed');
 			});
 
-		if (!authData.record) {
+		if (authData.token) {
+			// Add user to waitlist after admin authentication
+			await locals.pb.collection('waitlist').create({
+				email
+			});
+
+			return {
+				status: 200,
+				body: { success: true }
+			};
+		} else {
 			throw error(403, 'Authentication failed');
 		}
-
-		// Add user to waitlist after admin authentication
-		await locals.pb.collection('waitlist').create({
-			email
-		});
-
-		return {
-			status: 200,
-			body: { success: true }
-		};
 	}
 };
