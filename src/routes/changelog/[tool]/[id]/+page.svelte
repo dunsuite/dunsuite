e99f1800316af40a32ed246d0a6ffa9b1c8e3ee9
SpikeySanju <spikeysanju98@gmail.com>
2024-11-05 03:21:40 +0530

    Update image border radius in changelog for improved styling consistency

--- src/routes/changelog/[tool]/[id]/+page.svelte
+++ src/routes/changelog/[tool]/[id]/+page.svelte
@@ -48,7 +48,7 @@
 			'prose prose-lg flex flex-col duration-800 animate-in slide-in-from-bottom-96',
 			'prose-p:text-xl prose-p:font-medium',
 			'prose-a:text-xl prose-a:font-medium prose-a:underline',
-			'prose-img:rounded-xl prose-img:border-2 prose-img:border-white',
+			'prose-img:rounded-3xl prose-img:border-2 prose-img:border-white',
 			'prose-h1:font-medium',
 			'prose-h2:font-medium',
 			'prose-h3:font-medium',
