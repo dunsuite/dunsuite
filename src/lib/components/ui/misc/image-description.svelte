bb2ddcc88b1c77db89ca4aca87dd8cd48de8c815
Spikey Sanju <spikeysanju98@gmail.com>
2025-01-26 13:13:25 +0530

    Fix event handlers in image description component and adjust paragraph alignment

--- src/lib/components/ui/misc/image-description.svelte
+++ src/lib/components/ui/misc/image-description.svelte
@@ -9,8 +9,8 @@
 
 <div 
     class="absolute left-3 bottom-3 right-3 opacity-0 group-hover/image:opacity-100 transition-opacity duration-200"
-    on:mouseenter={() => isHovered = true}
-    on:mouseleave={() => isHovered = false}
+    onmouseenter={() => isHovered = true}
+    onmouseleave={() => isHovered = false}
 >
     <div 
         class={`flex flex-col bg-black/40 text-white text-sm rounded-xl p-3 gap-3 backdrop-blur-md shadow-lg ${isHovered?"w-full lg:w-[340px]":"w-fit"}`}
@@ -28,7 +28,7 @@
                     duration: 0,
                     easing: quintOut
                 }}
-                class="w-full leading-normal"
+                class="w-full leading-normal text-start"
             >
                 {description}
             </p>
